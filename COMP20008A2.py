3993992e2249f00b06b0b96427fb5ef999885b16
Weiyan Chneg <903105509@qq.com>
2023-10-02 15:17:26 +1100

    clean

--- COMP20008A2.py
+++ COMP20008A2.py
@@ -1,17 +1,19 @@
 import pandas as pd
 
 # read data
-credit = pd.read_csv('credits.csv', encoding='ISO-8859-1')
-titles = pd.read_csv('titles.csv', encoding='ISO-8859-1')
+credit = pd.read_csv('E:/20008/ASS2/credits.csv', encoding='ISO-8859-1')
+titles = pd.read_csv('E:/20008/ASS2/titles.csv', encoding='ISO-8859-1')
 
 # print(credit.info())
 # print(titles.info())
 
 # drop na
 credit.dropna(inplace=True)
+titles.drop(columns=['seasons'], inplace=True)
 columns_to_check = ['imdb_id', 'imdb_score', 'imdb_votes', 'tmdb_popularity', 'tmdb_score']
 titles.dropna(subset=columns_to_check, inplace=True)
 titles.to_csv('E:/20008/ASS2/titles.csv', index=False)
+
 # Delete duplicate value
 credit.drop_duplicates(inplace=True)
 titles.drop_duplicates(inplace=True)
