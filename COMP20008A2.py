e9a1b46717a2fe2fb8f9027430d7c37bc3c2c405
XUHENGL <1064921660@qq.com>
2023-10-02 15:56:31 +1100

    fit regression model

--- COMP20008A2.py
+++ COMP20008A2.py
@@ -1,16 +1,18 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from collections import Counter
 import ast
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
 
 # read data
 credit = pd.read_csv('credits.csv', encoding='ISO-8859-1')
 titles = pd.read_csv('titles.csv', encoding='ISO-8859-1')
 
-# print(credit.info())
-# print(titles.info())
-
-# drop na
+# drop na and variable seasons
 credit.dropna(inplace=True)
 # titles.drop(columns=['seasons'], inplace=True)
 columns_to_check = ['imdb_id', 'imdb_score', 'imdb_votes', 'tmdb_popularity', 'tmdb_score']
@@ -21,68 +23,36 @@ titles.to_csv('titles.csv', index=False)
 credit.drop_duplicates(inplace=True)
 titles.drop_duplicates(inplace=True)
 
+# change genres type
+titles['genres'] = titles['genres'].apply(ast.literal_eval)
 
-# ------------------------------------------------------------------------------------------------------------
-# The film is divided into three categories: Short film, Medium-length film and Feature film. And count the number
-
-# Filter out the data whose type is 'movie'
-movie_titles = titles[titles['type'] == 'MOVIE'].copy()
-
-# Creates a new column to store the category of the movie
-def categorize_runtime(runtime):
-    if runtime < 40:
-        return 'Short movie'
-    elif 40 <= runtime <= 70:
-        return 'Medium-length movie'
-    else:
-        return 'Feature movie'
-
-movie_titles['category'] = movie_titles['runtime'].apply(categorize_runtime)
-
-# Count the number of movies in each category
-category_counts = movie_titles['category'].value_counts()
-
-ax = category_counts.plot(kind='bar', color=['blue', 'orange', 'green'])
-plt.title('Distribution of Movie Lengths')
-plt.xlabel('Category')
-plt.ylabel('Number of Movies')
-plt.xticks(rotation=0)
-
-for p in ax.patches:
-    ax.annotate(str(p.get_height()),
-                (p.get_x() + p.get_width() / 2., p.get_height()),
-                ha='center', va='center',
-                xytext=(0, 9),
-                textcoords='offset points')
-
-plt.show()
+# print(credit.info())
+# print(titles.info())
 
 
 # ------------------------------------------------------------------------------------------------------------
 
-# Movies are divided into five categories: G,NC-17,PG, and PG-13. And count the numbers.
 
-# Filter out the data whose type is 'movie'
-movie_titles = titles[titles['type'] == 'MOVIE'].copy()
+features = ['release_year', 'runtime', 'tmdb_popularity', 'tmdb_score', 'genres', 'production_countries']
+target = 'imdb_score'
 
-# Filters out the specified five age_certification categories
-certifications = ['G', 'NC-17', 'PG', 'PG-13', 'R']
-movie_titles = movie_titles[movie_titles['age_certification'].isin(certifications)]
+X = titles[features]
+Y = titles[target]
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=666)
 
-# Count the number of movies in each category
-certification_counts = movie_titles['age_certification'].value_counts()
+numeric_features = ['release_year', 'runtime', 'tmdb_popularity', 'tmdb_score']
+numeric_transformer = SimpleImputer(strategy='median')
 
-ax = certification_counts.plot(kind='bar', color=['blue', 'orange', 'green', 'red', 'purple'])
-plt.title('Distribution of Age Certifications')
-plt.xlabel('Certification')
-plt.ylabel('Number of Movies')
-plt.xticks(rotation=0)
+categorical_features = ['genres', 'production_countries']
+categorical_transformer = Pipeline(steps=[
+    ('impute', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
 
-for p in ax.patches:
-    ax.annotate(str(p.get_height()),
-                (p.get_x() + p.get_width() / 2., p.get_height()),
-                ha='center', va='center',
-                xytext=(0, 9),
-                textcoords='offset points')
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)
+    ])
 
-plt.show()
+print(titles.dtypes)
