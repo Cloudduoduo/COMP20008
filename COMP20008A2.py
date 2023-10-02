a0fd4731a3343e88c3866a1f02da52f62069393a
Weiyan Chneg <903105509@qq.com>
2023-10-02 14:37:58 +1100

    clean

--- COMP20008A2.py
+++ COMP20008A2.py
@@ -1,24 +1,23 @@
 import pandas as pd
-import pandas as ps
 
-credits = pd.read_csv('E:/20008/ASS2/credits.csv')
-titles = pd.read_csv('E:/20008/ASS2/titles.csv')
+credit = pd.read_csv('E:/20008/ASS2/credits.csv')
+title = pd.read_csv('E:/20008/ASS2/titles.csv')
 
-print(credits.info())
-print(titles.info())
+print(credit.info())
+print(title.info())
 
 # drop na
-credits.dropna(inplace=True)
-titles.dropna(inplace=True)
+credit.dropna(inplace=True)
+title.dropna(inplace=True)
 
 # Delete duplicate value
-credits.drop_duplicates(inplace=True)
-titles.drop_duplicates(inplace=True)
+credit.drop_duplicates(inplace=True)
+title.drop_duplicates(inplace=True)
 # Sort the roles in the credits file
-sorted_credits = credits.sort_values(by='role', ascending=True)
+sorted_credits = credit.sort_values(by='role', ascending=True)
 
-
-
+merged_data = pd.merge(credit, title, on='id')
+merged_data.to_csv('merged_data.csv', index=False)
 
 
 
