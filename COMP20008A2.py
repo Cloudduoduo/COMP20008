eac97dbf45d4ee77142be777365f559b21eebc1f
Weiyan Chneg <903105509@qq.com>
2023-10-02 14:36:04 +1100

    clean

--- COMP20008A2.py
+++ COMP20008A2.py
@@ -1,6 +1,88 @@
 import pandas as pd
 import pandas as ps
 
-credits = pd.read_csv('credits.csv')
-titles = pd.read_csv('titles.csv')
+credits = pd.read_csv('E:/20008/ASS2/credits.csv')
+titles = pd.read_csv('E:/20008/ASS2/titles.csv')
+
+print(credits.info())
+print(titles.info())
+
+# drop na
+credits.dropna(inplace=True)
+titles.dropna(inplace=True)
+
+# Delete duplicate value
+credits.drop_duplicates(inplace=True)
+titles.drop_duplicates(inplace=True)
+# Sort the roles in the credits file
+sorted_credits = credits.sort_values(by='role', ascending=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
