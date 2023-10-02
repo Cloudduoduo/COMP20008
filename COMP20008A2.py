67c55c764119aaec3dc45c793de2930c2fec0899
XUHENGL <1064921660@qq.com>
2023-10-02 14:40:21 +1100

    1

--- COMP20008A2.py
+++ COMP20008A2.py
@@ -1,2 +1,89 @@
-print("sb")
-print("hi")
+import pandas as pd
+
+# read data
+credit = pd.read_csv('credits.csv', encoding='ISO-8859-1')
+titles = pd.read_csv('titles.csv', encoding='ISO-8859-1')
+
+print(credit.info())
+print(titles.info())
+
+# drop na
+credit.dropna(inplace=True)
+titles.dropna(inplace=True)
+
+# Delete duplicate value
+credit.drop_duplicates(inplace=True)
+titles.drop_duplicates(inplace=True)
+
+# Sort the roles in the credits file
+sorted_credits = credit.sort_values(by='role', ascending=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
