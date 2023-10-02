4d29d18044358201ffac6118df550fd6d1c34cf2
XUHENGL <1064921660@qq.com>
2023-10-02 15:09:41 +1100

    check 种类

--- COMP20008A2.py
+++ COMP20008A2.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import ast
+
 
 # read data
 credit = pd.read_csv('credits.csv', encoding='ISO-8859-1')
@@ -17,3 +19,20 @@ titles.drop_duplicates(inplace=True)
 
 
 # ------------------------------------------------------------------------------------------------------------
+
+all_genres = []
+
+# 遍历dataframe中的每一行
+for index, row in titles.iterrows():
+    # 将字符串转换为列表
+    genres_list = ast.literal_eval(row['genres'])
+
+    # 将这一行中的每个genre添加到all_genres列表中
+    all_genres.extend(genres_list)
+
+# 使用set来找到all_genres列表中唯一的genres
+unique_genres = set(all_genres)
+
+# 打印出结果
+print("Number of unique genres:", len(unique_genres))
+print("List of unique genres:", unique_genres)
