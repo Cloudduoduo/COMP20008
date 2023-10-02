0d15ff82b6e3821be3d5295d42d86b75cc4f6d43
XUHENGL <1064921660@qq.com>
2023-10-02 15:23:04 +1100

    更改genres 类型

--- COMP20008A2.py
+++ COMP20008A2.py
@@ -1,22 +1,24 @@
 import pandas as pd
+import ast
 
 # read data
-credit = pd.read_csv('E:/20008/ASS2/credits.csv', encoding='ISO-8859-1')
-titles = pd.read_csv('E:/20008/ASS2/titles.csv', encoding='ISO-8859-1')
+credit = pd.read_csv('credits.csv', encoding='ISO-8859-1')
+titles = pd.read_csv('titles.csv', encoding='ISO-8859-1')
 
-# print(credit.info())
-# print(titles.info())
-
-# drop na
+# drop na and variable seasons
 credit.dropna(inplace=True)
-titles.drop(columns=['seasons'], inplace=True)
+# titles.drop(columns=['seasons'], inplace=True)
 columns_to_check = ['imdb_id', 'imdb_score', 'imdb_votes', 'tmdb_popularity', 'tmdb_score']
 titles.dropna(subset=columns_to_check, inplace=True)
-titles.to_csv('E:/20008/ASS2/titles.csv', index=False)
+titles.to_csv('titles.csv', index=False)
 
 # Delete duplicate value
 credit.drop_duplicates(inplace=True)
 titles.drop_duplicates(inplace=True)
 
+# change genres type
+titles['genres'] = titles['genres'].apply(ast.literal_eval)
 
+# print(credit.info())
+# print(titles.info())
 # ------------------------------------------------------------------------------------------------------------
